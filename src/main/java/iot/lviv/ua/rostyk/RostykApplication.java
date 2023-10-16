72d990eb2b1137a58f267152e6d3e40a5dd62ca1
RoSteik <rostyslav.postolaki@gmail.com>
2023-10-16 21:41:07 +0300

    some commitb

--- src/main/java/iot/lviv/ua/rostyk/RostykApplication.java
+++ src/main/java/iot/lviv/ua/rostyk/RostykApplication.java
@@ -2,8 +2,10 @@ package iot.lviv.ua.rostyk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RostykApplication {
 
     public static void main(String[] args) {
